7ba25dc70c9d7cdb67f0d1814cd3e1a178776bad
toucansam-bpl <toucansam@blockpool.io>
2019-06-04 16:58:04 -0500

    Deployer now adds premine transaction

--- packages/js-deployer/src/genesis-block-generator.js
+++ packages/js-deployer/src/genesis-block-generator.js
@@ -1,3 +1,5 @@
+const { Bignum, crypto, } = require('@arkecosystem/crypto')
+
 const generateSequence = require('./sequence-generator')
 
 const delegateFactory = require('./delegate-factory')
@@ -5,12 +7,15 @@ const generatePassphrase = require('./passphrase-generator')
 const genesisBlockFactory = require('./genesis-block-factory')
 const walletFactory = require('./wallet-factory')
 
-module.exports = ({ delegateCount, passphraseFilePath, keyMapFilePath }) => {
+module.exports = ({ delegateCount, passphraseFilePath, keyMapFilePath, totalPremine, }) => {
   const createDelegate = delegateFactory(passphraseFilePath, keyMapFilePath)
   const passphrases = generatePassphrase()
   const createWallet = walletFactory(() => ({ passphrase: passphrases.next().value }))
 
   const genesisWallet = createWallet()
+  const premineWallet = createWallet()
+  const premineTransfer = createTransferTransaction(premineWallet, genesisWallet, Bignum(totalPremine))
+
   const delegates = generateSequence(delegateCount).map(createDelegate)
   const createGenesisBlock = genesisBlockFactory({
     genesisWallet,
@@ -19,7 +24,32 @@ module.exports = ({ delegateCount, passphraseFilePath, keyMapFilePath }) => {
 
   return {
     delegatePassphrases: delegates.map(d => d.passphrase),
-    genesisBlock: createGenesisBlock(delegates.map(d => d.transaction)),
+    genesisBlock: createGenesisBlock(delegates.map(d => d.transaction).concat([premineTransfer])),
     genesisWallet,
   }
+}
+
+
+const createTransferTransaction = (senderWallet, receiverWallet, amount) => {
+  const { data } = client
+    .getBuilder()
+    .transfer()
+    .recipientId(receiverWallet.address)
+    .amount(amount)
+    .network(this.prefixHash)
+    .sign(senderWallet.passphrase)
+
+  return formatGenesisTransaction(data, senderWallet)
+}
+
+const formatGenesisTransaction = (transaction, wallet) => {
+  Object.assign(transaction, {
+    fee: Bignum(0),
+    timestamp: 0,
+    senderId: wallet.address,
+  })
+  transaction.signature = crypto.sign(transaction, wallet.keys)
+  transaction.id = crypto.getId(transaction)
+
+  return transaction
 }
