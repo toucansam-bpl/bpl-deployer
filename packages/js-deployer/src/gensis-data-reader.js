8721810d0d9d6762c3c9ec6d5b4ec3bf2ee61ad2
toucansam-bpl <toucansam@blockpool.io>
2019-06-28 15:06:02 -0500

    Added delegate registration refund and second passphrase refund queries

--- packages/js-deployer/src/gensis-data-reader.js
+++ packages/js-deployer/src/gensis-data-reader.js
@@ -6,8 +6,12 @@ module.exports = () =>
     new Promise(async (resolve, reject) => {
         try {
             await client.connect()
-            const voteRefundAddresses = await getVoteRefundAddresses()
+            const delegateRegistrationRefundAddresses = await runAddressQuery(delegateRegistrationRefundAddressQuery)
+            const secondPassphraseRefundAddresses = await runAddressQuery(secondPassphraseRefundAddressQuery)
+            const voteRefundAddresses = await runAddressQuery(voteRefundAddressQuery)
             resolve({
+                delegateRegistrationRefundAddresses,
+                secondPassphraseRefundAddresses,
                 voteRefundAddresses,
             })
         }
@@ -19,40 +23,43 @@ module.exports = () =>
         }
     })
 
-const getVoteRefundAddresses = async () => new Promise(async (resolve, reject) => {
-    try {
-        const result = await client.query(`SELECT a.address, v.votes
-            FROM mem_accounts a
-            JOIN (
-                SELECT "senderPublicKey", count(1) as votes
-                FROM transactions
-                WHERE type = 3
-                GROUP BY "senderPublicKey"
-                HAVING count(1) % 2 = 1
-            ) v
-                ON a."publicKey" = v."senderPublicKey";
-            `
-        )
-        return result.rows.map(r => r.address)
-    }
-})
+const runAddressQuery = queryText =>
+    async () => new Promise(async (resolve, reject) => {
+        try {
+            const result = await client.query(queryText)
+            resolve(result.rows.map(r => r.address))
+        }
+        catch (ex) {
+            reject(ex)
+        }
+    })
 
-/*
-SELECT a.address
+const voteRefundAddressQuery = `SELECT a.address, v.votes
+    FROM mem_accounts a
+    JOIN (
+        SELECT "senderPublicKey", count(1) as votes
+        FROM transactions
+        WHERE type = 3
+        GROUP BY "senderPublicKey"
+        HAVING count(1) % 2 = 1
+    ) v
+        ON a."publicKey" = v."senderPublicKey"
+;`
+
+const delegateRegistrationRefundAddressQuery = `SELECT a.address
   FROM mem_accounts a
   JOIN (
     SELECT "senderPublicKey" FROM transactions WHERE type = 2 AND rawasset NOT LIKE '%BPL_MC_%'
   ) t
     ON a."publicKey" = t."senderPublicKey"
-*/
+;`
 
-/*
-SELECT a.address
+
+const secondPassphraseRefundAddressQuery = `SELECT a.address
   FROM mem_accounts a
   JOIN (
     SELECT "senderPublicKey" FROM transactions WHERE type = 1
   ) t
     ON a."publicKey" = t."senderPublicKey"
-;
-*/
+;`
 
